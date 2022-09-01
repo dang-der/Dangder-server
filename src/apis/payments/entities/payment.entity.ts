9eb394b5dd5b7cdbf3bcd4739b8e84785143bb37
poagg42 <poagg42@gmail.com>
2022-09-01 15:02:09 +0900

    :fire: 220901 - modify : entity 15:00 수정

--- src/apis/payments/entities/payment.entity.ts
+++ src/apis/payments/entities/payment.entity.ts
@@ -1,5 +1,18 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { User } from 'src/apis/users/entities/user.entity';
+import { PAYMENT_TYPE_ENUM } from 'src/commons/type/enums';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+registerEnumType(PAYMENT_TYPE_ENUM, {
+  // 이것은 graphql에 등록하기 위해
+  name: 'PAYMENT_TYPE_ENUM',
+});
 
 @Entity()
 @ObjectType()
@@ -7,4 +20,17 @@ export class Payment {
   @PrimaryGeneratedColumn('uuid')
   @Field(() => String)
   id: string;
+
+  @Column()
+  @Field(() => Int)
+  payMoney: number;
+
+  @Column({ type: 'enum', enum: PAYMENT_TYPE_ENUM })
+  @Field(() => PAYMENT_TYPE_ENUM)
+  paymentType: string;
+
+  @JoinColumn()
+  @Field(() => User)
+  @OneToOne(() => User)
+  user: User;
 }
