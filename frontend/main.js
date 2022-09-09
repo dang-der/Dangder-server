f2cddd3a1d2a5a953f69b3b2b683e03e9be00311
banssss <watermelonzv@gmail.com>
2022-09-09 12:45:05 +0900

    :fire: 220909 - feat : webSocket을 이용한 채팅 테스트 수정(작업중)

--- frontend/main.js
+++ frontend/main.js
@@ -1,23 +1,43 @@
 const app = new Vue({
   el: '#app',
   data: {
-    title: 'Nestjs Websockets Chat',
+    title: 'Nestjs Websockets 채팅 무한삽질',
+    room: '',
     name: '',
     text: '',
+    welcomes: [],
     messages: [],
     socket: null,
   },
   methods: {
+    changeChatRoom() {
+      const room = prompt('입장할 채팅방의 코드를 입력하세요');
+      this.room = room;
+      this.welcomes = [];
+      this.messages = [];
+      this.text = '';
+      const message = {
+        room: this.room,
+        name: this.name,
+      };
+      this.initChatRoom(message);
+    },
+    initChatRoom(message) {
+      this.socket.emit('init', message);
+    },
     sendMessage() {
       if (this.validateInput()) {
         const message = {
-          room: 'chatRoomId',
+          room: this.room,
           name: this.name,
           text: this.text,
         };
-        this.socket.emit('msgToServer', message);
+        this.socket.emit('send', message);
         this.text = '';
       }
+    },
+    receivedJoin(welcome) {
+      if (!this.welcomes.includes(welcome)) this.welcomes.push(welcome);
     },
     receivedMessage(message) {
       this.messages.push(message);
@@ -28,7 +48,13 @@ const app = new Vue({
   },
   created() {
     this.socket = io('http://localhost:3000/chats');
-    this.socket.on('msgToClient', (message) => {
+    this.socket.on('init', (welcome) => {
+      this.initChatRoom(welcome);
+    });
+    this.socket.on('join', (welcome) => {
+      this.receivedJoin(welcome);
+    });
+    this.socket.on('sendToWindow', (message) => {
       this.receivedMessage(message);
     });
   },
