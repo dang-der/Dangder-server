dd1cd0e1a52475496b3d52b54ccbc6be6d31b0a3
banssss <watermelonzv@gmail.com>
2022-09-21 23:33:27 +0900

    :fire: 220921 - refactor : fetchOneDog img order DESC setting

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,11 @@ import { graphqlUploadExpress } from 'graphql-upload';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
 
-// Chat 을 위한 임시 import
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(graphqlUploadExpress()); // graphql upload 이용하기위한 추가
-  app.useStaticAssets(join(__dirname, '..', 'frontend')); // socket test 를 위한 추가
   // Cors 추가
   app.enableCors({
     origin: ['https://dangder.shop', 'http://localhost:3000'],
