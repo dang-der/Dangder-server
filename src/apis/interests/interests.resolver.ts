34f8de1d6c57aa2a91a81a3d57272ddcc98a202e
banssss <watermelonzv@gmail.com>
2022-10-26 18:01:08 +0900

    :fire: 221026 - refactor : Stop유저 로그인차단 등

--- src/apis/interests/interests.resolver.ts
+++ src/apis/interests/interests.resolver.ts
@@ -39,9 +39,7 @@ export class InterestsResolver {
   ) {
     const result = await this.interestsService.create(createInterestInput); //
     // 관심사 생성 시, 관심사와 1:1로 연결된 관심사 채팅방 생성
-    console.log('======0========');
     await this.interestChatRoomsService.create({ interest: { id: result.id } });
-    console.log('======1========');
     return result;
   }
 
